c789d61334090a43e5512a396e815eacb367880c
ichlieb <80103127+ichlieb@users.noreply.github.com>
2021-03-16 16:19:39 +0300

    Update two_rows.m

--- two_rows.m
+++ two_rows.m
@@ -2,10 +2,6 @@ function a = two_rows(m)
 b = size(m);
 if ndims(m) == 2 && b(1) == 2
         a = m;
-    else
-      a = zeros(size(m));
-      disp("Массив должен быть двумерным и иметь две строки");
-    end
 else
     a = zeros(size(m));
     disp("Массив должен быть двумерным и иметь две строки");
