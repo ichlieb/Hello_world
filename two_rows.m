7420916e84383b89fcb532ad35e7b118baa1ea39
ichlieb <80103127+ichlieb@users.noreply.github.com>
2021-12-16 23:00:58 +0300

    Delete two_rows.m

--- two_rows.m
+++
@@ -1,9 +0,0 @@
-function a = two_rows(m)
-b = size(m);
-if ndims(m) == 2 && b(1) == 2
-        a = m;
-else
-    a = zeros(size(m));
-    disp("Массив должен быть двумерным и иметь две строки");
-end
-    
